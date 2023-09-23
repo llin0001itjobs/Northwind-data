214d33bc570dbb9c17ac747e1a6285eb3dd99ae6
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-09-23 13:22:18 -0400

    Add files via upload

--- src/main/java/org/llin/demo/northwind/NorthwindApplication.java
+++ src/main/java/org/llin/demo/northwind/NorthwindApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NorthwindApplication extends SpringBootServletInitializer {
 
 	@Override
