b6cf205c170cf308f54029a194e841f78a383ee6
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2024-11-12 18:33:07 -0500

    Add files via upload

--- src/main/resources/sql-scripts/northwind_authentication.sql
+++ src/main/resources/sql-scripts/northwind_authentication.sql
@@ -1,5 +1,15 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: northwind
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+--
+-- Table structure for table authentication
+--
 
 DROP TABLE IF EXISTS authentication;
+
 CREATE TABLE authentication (
   id int NOT NULL,
   role_type varchar(64) NOT NULL DEFAULT '0',
@@ -18,7 +28,4 @@ LOCK TABLES authentication WRITE;
 INSERT INTO authentication VALUES (1,'USER','MyrTkkFOL5AHSQfZhzo74w==','o7Q1KoXVrH7WvEMUoMI1gA=='),(2,'USER','VTGN1ljR6fbXlborxtZzBQ==','VTGN1ljR6fbXlborxtZzBQ==');
 
 UNLOCK TABLES;
-
-
-
 
