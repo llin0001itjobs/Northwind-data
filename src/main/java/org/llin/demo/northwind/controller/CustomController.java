ece73a3e076165ee5e673a5eb88f851d88e10cb2
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2024-12-13 09:23:19 -0500

    Add files via upload

--- src/main/java/org/llin/demo/northwind/controller/CustomController.java
+++ src/main/java/org/llin/demo/northwind/controller/CustomController.java
@@ -5,13 +5,10 @@ import java.util.List;
 import org.llin.demo.northwind.data.repository.CustomerOrderRepository;
 import org.llin.demo.northwind.data.repository.ProductRepository;
 import org.llin.demo.northwind.data.repository.PurchaseOrderDetailRepository;
-import org.llin.demo.northwind.data.repository.model.CategoryRatio;
-import org.llin.demo.northwind.data.repository.model.FeeRangePerCount;
-import org.llin.demo.northwind.data.repository.model.PriceRangePerListPrice;
-import org.llin.demo.northwind.data.repository.model.PriceRangePerStandardCost;
-import org.llin.demo.northwind.data.repository.model.QuantityPerCostRange;
-import org.llin.demo.northwind.data.repository.model.QuantityPerUnitCost;
-import org.llin.demo.northwind.data.repository.model.ShippingFeePerMonth;
+import org.llin.demo.northwind.data.repository.model.LabelDoubleValueLong;
+import org.llin.demo.northwind.data.repository.model.LabelIntValueDouble;
+import org.llin.demo.northwind.data.repository.model.LabelValueLong;
+import org.llin.demo.northwind.data.repository.model.LabelValueLongValueDouble;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,57 +19,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class CustomController {
 
-    @Autowired
-    private CustomerOrderRepository customerOrderRepository;
-    
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private CustomerOrderRepository customerOrderRepository;
 
-    @Autowired
-    private PurchaseOrderDetailRepository purchaseOrderDetailRepository;
-    
-    @GetMapping("/customerOrder/shippingFeePerMonth")
-    public ResponseEntity<List<ShippingFeePerMonth>> getShippingFeePerMonth() {
-        List<ShippingFeePerMonth> quantity = customerOrderRepository.shippingFeePerMonth();
-        return ResponseEntity.ok(quantity);
-    }    
-    
-    @GetMapping("/customerOrder/feeRangePerCount")
-    public ResponseEntity<List<FeeRangePerCount>> getFeeRangePerCount() {
-        List<FeeRangePerCount> priceRange = customerOrderRepository.feeRangePerCount();
-        return ResponseEntity.ok(priceRange);
-    }  
-    
-    @GetMapping("/product/categoryRatios")
-    public ResponseEntity<List<CategoryRatio>> getCategoryRatios() {
-        List<CategoryRatio> ratios = productRepository.categoryRatios();
-        return ResponseEntity.ok(ratios);
-    }
-    
-    @GetMapping("/product/priceRangePerStandardCost")
-    public ResponseEntity<List<PriceRangePerStandardCost>> getPriceRangePerStandardCost() {
-        List<PriceRangePerStandardCost> priceRange = productRepository.priceRangePerStandardCost();
-        return ResponseEntity.ok(priceRange);
-    }
-       
-    @GetMapping("/product/priceRangePerListPrice")
-    public ResponseEntity<List<PriceRangePerListPrice>> getPriceRangePerListPrice() {
-        List<PriceRangePerListPrice> priceRange = productRepository.priceRangePerListPrice();
-        return ResponseEntity.ok(priceRange);
-    }
-           
-    
-    @GetMapping("/purchaseOrderDetail/quantityPerUnitCost")
-    public ResponseEntity<List<QuantityPerUnitCost>> getQuantityPerUnitCost() {
-        List<QuantityPerUnitCost> quantity = purchaseOrderDetailRepository.quantityPerUnitCost();
-        return ResponseEntity.ok(quantity);
-    }    
-    
-    @GetMapping("/purchaseOrderDetail/quantityPerCostRange")
-    public ResponseEntity<List<QuantityPerCostRange>> getQuantityPerCostRange() {
-        List<QuantityPerCostRange> priceRange = purchaseOrderDetailRepository.quantityPerCostRange();
-        return ResponseEntity.ok(priceRange);
-    }        
-    
-    
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private PurchaseOrderDetailRepository purchaseOrderDetailRepository;
+
+	@GetMapping("/customerOrder/feeRangePerCount")
+	public ResponseEntity<List<LabelValueLong>> getFeeRangePerCount() {
+		List<LabelValueLong> priceRange = customerOrderRepository.feeRangePerCount();
+		return ResponseEntity.ok(priceRange);
+	}
+
+	@GetMapping("/customerOrder/shippingFeePerMonth")
+	public ResponseEntity<List<LabelIntValueDouble>> getShippingFeePerMonth() {
+		List<LabelIntValueDouble> quantity = customerOrderRepository.shippingFeePerMonth();
+		return ResponseEntity.ok(quantity);
+	}
+
+	@GetMapping("/product/categoryRatios")
+	public ResponseEntity<List<LabelValueLongValueDouble>> getCategoryRatios() {
+		List<LabelValueLongValueDouble> ratios = productRepository.categoryRatios();
+		return ResponseEntity.ok(ratios);
+	}
+
+	@GetMapping("/product/priceRangePerListPrice")
+	public ResponseEntity<List<LabelValueLong>> getPriceRangePerListPrice() {
+		List<LabelValueLong> priceRange = productRepository.priceRangePerListPrice();
+		return ResponseEntity.ok(priceRange);
+	}
+
+	@GetMapping("/product/priceRangePerStandardCost")
+	public ResponseEntity<List<LabelValueLong>> getPriceRangePerStandardCost() {
+		List<LabelValueLong> priceRange = productRepository.priceRangePerStandardCost();
+		return ResponseEntity.ok(priceRange);
+	}
+
+	@GetMapping("/purchaseOrderDetail/quantityPerCostRange")
+	public ResponseEntity<List<LabelValueLong>> getQuantityPerCostRange() {
+		List<LabelValueLong> priceRange = purchaseOrderDetailRepository.quantityPerCostRange();
+		return ResponseEntity.ok(priceRange);
+	}
+
+	@GetMapping("/purchaseOrderDetail/quantityPerUnitCost")
+	public ResponseEntity<List<LabelDoubleValueLong>> getQuantityPerUnitCost() {
+		List<LabelDoubleValueLong> quantity = purchaseOrderDetailRepository.quantityPerUnitCost();
+		return ResponseEntity.ok(quantity);
+	}
+
 }
